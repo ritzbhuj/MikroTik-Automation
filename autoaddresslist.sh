f28ac5427c6049a0c177e0f2a237750154be8d83
Ritesh Pritamlall Bhujun <ritzbhuj@gmail.com>
2020-09-16 13:29:55 +0200

    Changed to for loop

--- autoaddresslist.sh
+++ autoaddresslist.sh
@@ -8,9 +8,10 @@ mikUsername="XXXXXXX" #->Mikrouterusername
 mikPassword="XXXXXXX" #->Mikrouterpassword
 mikIP="192.xxx.xxx.xxx" # IP address of mik router
 #address=#placemarker
-list="addresslist" # -> Your address list name on mik router
+list="priority-web" # -> Your address list name on mik router
 ####################
-while read address;
+for address in $(cat populatelist);
 do
-    sshpass -p ${mikPassword} ssh -o StrictHostKeyChecking=no ssh ${mikUsername}@${mikIP} ip firewall address list add address=${address} list=${list};
-done < populatelist
+    echo -e ${address};
+    sshpass -p ${mikPassword} ssh -o StrictHostKeyChecking=no -p 2222 ${mikUsername}@${mikIP} ip firewall address-list add address=${address} list=${list};
+done
